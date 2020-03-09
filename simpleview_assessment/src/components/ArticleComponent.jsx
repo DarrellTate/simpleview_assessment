09ddb3d8074faeab629eb2ebc1700da0dac909a6
DarrellTate <DarrellTate@users.noreply.github.com>
2020-03-08 23:42:58 -0700

    update pages to use data from api

--- simpleview_assessment/src/components/ArticleComponent.jsx
+++ simpleview_assessment/src/components/ArticleComponent.jsx
@@ -5,7 +5,51 @@ class ArticleComponent extends Component {
   constructor(props) {
     super();
     this.state = {
-      article: props.article
+      imageurl:
+        props.article && props.article.mediaurl
+          ? props.article.mediaurl
+          : fallback_image,
+      title:
+        props.article && props.article.title
+          ? props.article.title
+          : "Title Unavailable",
+      zip:
+        props.article && props.article.zip
+          ? props.article.zip
+          : "Zip Unavailable",
+      address1:
+        props.article && props.article.address1
+          ? props.article.address
+          : "Address Unavailable",
+      region:
+        props.article && props.article.region
+          ? props.article.region
+          : "Region Unavailable",
+      description:
+        props.article && props.article.description
+          ? props.article.description
+          : "Description Unavailable",
+      city:
+        props.article && props.article.city
+          ? props.article.city
+          : "City Unavailable",
+      state:
+        props.article && props.article.state
+          ? props.article.state
+          : "State Unavailable",
+      recid: props.article && props.article.recid ? props.article.recid : -1,
+      weburl:
+        props.article && props.article.weburl
+          ? props.article.weburl
+          : "WebUrl Unavailable",
+      phone:
+        props.article && props.article.phone
+          ? props.article.phone
+          : "Phone Unavailable",
+      qualityScore:
+        props.article && props.article.qualityScore
+          ? props.article.qualityScore
+          : "City Unavailable"
     };
   }
 
@@ -13,12 +57,14 @@ class ArticleComponent extends Component {
     return (
       <article>
         <div className="article-img-container">
-          <img src={fallback_image} alt="img" height="100%" width="100%" />
+          <img src={this.state.imageurl} alt="img" height="100%" width="100%" />
           <div className="article-text-container">
             <div className="article-title-container">
-              <h3>Some Title</h3>
+              <h3>{this.state.title}</h3>
             </div>
-            <p className="artice-description-container">Some Description</p>
+            <p className="artice-description-container">
+              {this.state.description}
+            </p>
           </div>
         </div>
       </article>
--- simpleview_assessment/src/components/MainComponent.jsx
+++ simpleview_assessment/src/components/MainComponent.jsx
@@ -2,51 +2,41 @@ import React, { Component } from "react";
 import ArticleComponent from "./ArticleComponent";
 
 class MainComponent extends Component {
-  state = {};
+  state = {
+    loading: false,
+    listings: []
+  };
 
   componentDidMount() {
+    this.setState({ loading: true });
     fetch("https://sv-reqres.now.sh/api/listings")
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => {
+        this.setState({ listings: data.data, loading: false });
+      });
   }
 
   render() {
+    const loading_text = 'The "listings" api is loading';
+
+    // to achieve the pattern I need to set every 5th article's lg col grid to 6
+    const listings = this.state.listings.map(listing => {
+      let indexValue = this.state.listings.indexOf(listing);
+      let listing_class_name = "col col-xs-12 col-sm-12 col-md-4 col-lg-";
+      listing_class_name +=
+        indexValue === 0 || indexValue % 5 === 0 ? "6" : "3";
+
+      return (
+        <div key={indexValue} className={listing_class_name}>
+          <ArticleComponent article={listing} />
+        </div>
+      );
+    });
+
     return (
       <main className="container">
         <div className="row">
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-6">
-            <ArticleComponent />
-          </div>
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-3">
-            <ArticleComponent />
-          </div>
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-3">
-            <ArticleComponent />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-3">
-            <ArticleComponent />
-          </div>
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-3">
-            <ArticleComponent />
-          </div>
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-6">
-            <ArticleComponent />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-6">
-            <ArticleComponent />
-          </div>
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-3">
-            <ArticleComponent />
-          </div>
-          <div className="col col-xs-12 col-sm-12 col-md-4 col-lg-3">
-            <ArticleComponent />
-          </div>
+          {this.state.loading ? loading_text : listings}
         </div>
       </main>
     );
