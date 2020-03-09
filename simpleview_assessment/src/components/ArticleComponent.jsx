5baaa548c8a0b7e92d378536277eed35fb122e88
DarrellTate <DarrellTate@users.noreply.github.com>
2020-03-09 00:56:14 -0700

    read more button

--- simpleview_assessment/src/components/ArticleComponent.jsx
+++ simpleview_assessment/src/components/ArticleComponent.jsx
@@ -56,15 +56,28 @@ class ArticleComponent extends Component {
   render() {
     return (
       <article>
+        {/*
+         The image needs to take up 50% of the width and 100% height if divisible
+        by 5 and not 10 ... I can accomplish this by using bootstrap grids
+        */}
         <div className="article-img-container">
           <img src={this.state.imageurl} alt="img" height="100%" width="100%" />
           <div className="article-text-container">
             <div className="article-title-container">
-              <h3>{this.state.title}</h3>
+              <strong>{this.state.title}</strong>
             </div>
-            <p className="artice-description-container">
-              {this.state.description}
-            </p>
+            <div class="panel-wrapper">
+              <a href="#show" class="show btn" id="show">
+                Read More
+              </a>
+              <a href="#hide" class="hide btn" id="hide">
+                Read Less
+              </a>
+              <p className="artice-description-container panel">
+                {this.state.description}
+              </p>
+              <div class="fade"></div>
+            </div>
           </div>
         </div>
       </article>
